d20438e38634481d93a091b5e827970c21ca0a74
VasaOfficial <VasaPofficial@gmail.com>
2023-01-12 14:55:21 +0100

    Add title and scoreboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import MemoryGame from "./components/Cards/Cards";
 
-function App() {}
+function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  return (
+    <div>
+      <MemoryGame currentScore={currentScore} bestScore={bestScore} />
+    </div>
+  );
+}
 
 export default App;
