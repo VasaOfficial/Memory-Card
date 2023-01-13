88175d0908541f1269edb79f6bff7492e19369d1
VasaOfficial <VasaPofficial@gmail.com>
2023-01-13 17:44:37 +0100

    Final commit

--- src/components/ScoreBoard/Score.jsx
+++ src/components/ScoreBoard/Score.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import "./scoreboard.css";
 
 const ScoreBoard = ({ currentScore, bestScore }) => {
   return (
     <>
-      <>
-        <div>
-          <h1>Memory Game</h1>
-        </div>
+      <div className="title">
+        <h1>Memory Game</h1>
+      </div>
+      <div className="score-display">
         <p>Current Score: {currentScore}</p>
-      </>
-      <p>Best Score: {bestScore}</p>
+        <p>Best Score: {bestScore}</p>
+      </div>
     </>
   );
 };
